84b2bc7d40dbbe3987dcfafd26e1056ebd895a18
chen yifei <113622306@qq.com>
2023-07-14 09:21:05 +0800

    update

--- Assets/Scripts/DamageReceiver.cs
+++ Assets/Scripts/DamageReceiver.cs
@@ -6,27 +6,38 @@ namespace Client
     public class DamageReceiver : MonoBehaviour
     {
         public LayerMask enemyLayer;
+        private SpriteRenderer spriteRenderer;
+        private Color originalColor;
+        private bool isDamaged;
 
-        private void OnCollisionEnter2D(Collision2D other)
+        private void Start()
         {
-            Debug.Log("SS Collision");
-            // 检查碰撞是否来自特定的层
-            if (enemyLayer == (enemyLayer | (1 << other.gameObject.layer)))
-            {
-                // 处理伤害逻辑
-                Debug.Log("受到伤害！");
-            }
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            originalColor = spriteRenderer.color;
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
-            Debug.Log("SS Trigger");
             // 检查碰撞是否来自特定的层
             if (enemyLayer == (enemyLayer | (1 << other.gameObject.layer)))
             {
                 // 处理伤害逻辑
                 Debug.Log("受到伤害！");
+
+                // 变红
+                if (!isDamaged)
+                {
+                    spriteRenderer.color = Color.red;
+                    isDamaged = true;
+                    Invoke("ResetColor", 0.2f); // 0.2 秒后恢复原色
+                }
             }
+        }
+
+        private void ResetColor()
+        {
+            spriteRenderer.color = originalColor;
+            isDamaged = false;
         }
     }
 }
